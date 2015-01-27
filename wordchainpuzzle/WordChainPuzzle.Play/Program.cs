194b765e83ec2caf4e67ef472fb3c21f3bb33a5a
selketjah <gien.verschatse@gmail.com>
2015-01-27 14:16:46 +0100

    added console to test

--- wordchainpuzzle/WordChainPuzzle.Play/Program.cs
+++ wordchainpuzzle/WordChainPuzzle.Play/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using WordChainPuzzle.Domain;
 
 namespace WordChainPuzzle.Play {
@@ -10,8 +11,19 @@ namespace WordChainPuzzle.Play {
       var wordChainPuzzle = new WordChain(dictionary);
 
       Console.WriteLine("Enter begin and target word (e.g. cat - dog)");
-      var words = Console.Read();
+      string words = Console.ReadLine();
 
+      var wordArray = words.Split('-');
+      if (dictionary.Contains(wordArray[0].Trim()) && dictionary.Contains(wordArray[1].Trim())) {
+        var chain = wordChainPuzzle.GetChain(wordArray[0].Trim(), wordArray[1].Trim());
+        Console.WriteLine("Word chain");
+        Console.WriteLine(chain);
+      }
+      else {
+        Console.WriteLine("Words not in dictionary");
+      }
+
+      Console.ReadLine();
 
     }
   }
