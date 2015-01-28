76bd33af6dbbb9f37e4928fad2dfe6d4f9b8701b
selketjah <gien.verschatse@gmail.com>
2015-01-28 15:37:55 +0100

    removed unused directives

---
+++ wordchainpuzzle/WordChainPuzzle.UI/content/js/WordChainWordComparer.js
@@ -0,0 +1,11 @@
+define(function(){
+	return function WordChainWordComparer() {
+		if(!(this instanceof WordChainWordComparer)){
+			return new WordChainWordComparer();
+		}
+
+		var self = this;
+
+		return self;
+	}
+})
