7386384f37061cfb98103387a9ec456d593c77be
selketjah <gien.verschatse@gmail.com>
2015-03-01 12:23:32 +0100

    refactor code

---
+++ Diamonds/Diamonds.Domain/MiddleDiamondPadding.cs
@@ -0,0 +1,42 @@
+﻿namespace Diamonds.Domain
+{
+  public class MiddleDiamondPadding
+  {
+    private readonly char _diamondLetter;
+    private AlphabeticalLetterConverter _alc;
+    private readonly int _diamondLetterValue;
+
+    public MiddleDiamondPadding(char diamondLetter)
+    {
+      _diamondLetter = diamondLetter;
+      _alc = new AlphabeticalLetterConverter();
+      _diamondLetterValue = _alc.GetAlphabeticalValue(diamondLetter);
+    }
+
+    public int Get(char letter)
+    {
+      var baseDiamondLetterValue = GetBaseValue();
+      var diamondLetterPositionValue = GetPositionValue(letter);
+
+      if (_diamondLetter.Equals(letter))
+      {
+        return baseDiamondLetterValue;
+      }
+
+      return baseDiamondLetterValue - (diamondLetterPositionValue * 2);
+    }
+
+    private int GetPositionValue(char letter)
+    {
+      var letterValue = _alc.GetAlphabeticalValue(letter);
+      var diamondLetterPositionValue = _diamondLetterValue - letterValue;
+      return diamondLetterPositionValue;
+    }
+
+    private int GetBaseValue()
+    {
+      var baseDiamondLetterValue = (_diamondLetterValue * 2) - 1;
+      return baseDiamondLetterValue;
+    }
+  }
+}
--- Diamonds/Diamonds.Tests/MiddleDiamondPaddingScenarios.cs
+++ Diamonds/Diamonds.Tests/MiddleDiamondPaddingScenarios.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Diamonds.Domain;
+﻿using Diamonds.Domain;
 using Xunit;
 
 namespace Diamonds.Tests {
@@ -26,35 +21,6 @@ namespace Diamonds.Tests {
       var middleDiamondPadding = new MiddleDiamondPadding(DiamondLetter);
       var result = middleDiamondPadding.Get(letter);
       Assert.Equal(5, result);
-    }
-  }
-
-  public class MiddleDiamondPadding
-  {
-    private readonly char _diamondLetter;
-    private AlphabeticalLetterConverter _alc;
-
-    public MiddleDiamondPadding(char diamondLetter)
-    {
-      _diamondLetter = diamondLetter;
-      _alc = new AlphabeticalLetterConverter();
-    }
-
-    public int Get(char letter)
-    {
-      var baseValue = 11;
-
-      if (_diamondLetter.Equals(letter))
-      {
-        return baseValue;
-      }
-
-      var diamondLetterValue = _alc.GetAlphabeticalValue(_diamondLetter);
-      var letterValue = _alc.GetAlphabeticalValue(letter);
-
-      var diamondLetterPositionValue = diamondLetterValue - letterValue;
-
-      return (diamondLetterValue * 2) - (diamondLetterPositionValue * 2) - 1;
     }
   }
 }
