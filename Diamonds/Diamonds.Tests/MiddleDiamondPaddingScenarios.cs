7b006901ecf47ec7a8046cec737c6714e05492cf
selketjah <gien.verschatse@gmail.com>
2015-03-01 12:06:28 +0100

    middle padding for f returns 11

---
+++ Diamonds/Diamonds.Tests/MiddleDiamondPaddingScenarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Diamonds.Tests {
+  public class MiddleDiamondPaddingScenarios {
+
+    [Fact]
+    public void char_F_returns_middlePadding_11()
+    {
+      var letter = 'F';
+      var middleDiamondPadding = new MiddleDiamondPadding();
+      var result = middleDiamondPadding.Get(letter);
+      Assert.Equal(11, result);
+    }
+  }
+
+  public class MiddleDiamondPadding
+  {
+    public int Get(char letter)
+    {
+      return 11;
+    }
+  }
+}
