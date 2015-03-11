e97fe8c5a8886b0d2b3b887bc3a6cb8cea3de863
selketjah <gien.verschatse@gmail.com>
2015-03-11 18:46:55 +0100

    added diamondletter

---
+++ Diamonds/Diamonds.Domain/DiamondLetter.cs
@@ -0,0 +1,26 @@
+﻿namespace Diamonds.Domain
+{
+  public class DiamondLetter
+  {
+    private readonly char _letter;
+    private readonly InnerDiamondPaddingCalculator _innerDiamondPaddingCalculator;
+    private readonly OuterDiamondPaddingCalculator _outerDiamondPaddingCalculator;
+
+    public DiamondLetter(char letter)
+    {
+      _letter = letter;
+      _innerDiamondPaddingCalculator = new InnerDiamondPaddingCalculator();
+      _outerDiamondPaddingCalculator = new OuterDiamondPaddingCalculator();
+    }
+
+    public int GetInnerPadding(char diamondLetter)
+    {
+      return _innerDiamondPaddingCalculator.Get(diamondLetter, _letter);
+    }
+
+    public int GetOuterPadding(char diamondLetter)
+    {
+      return _outerDiamondPaddingCalculator.Get(diamondLetter, _letter);
+    }
+  }
+}
---
+++ Diamonds/Diamonds.Tests/DiamondLetterScenarios.cs
@@ -0,0 +1,56 @@
+﻿using Diamonds.Domain;
+using Xunit;
+
+namespace Diamonds.Tests {
+  public class DiamondLetterScenarios {
+    private const char DiamondLetter = 'F';
+
+    [Fact]
+    public void char_F_returns_innerPadding_11() {
+      var letter = 'F';
+      var middleDiamondPadding = new DiamondLetter('F');
+      var result = middleDiamondPadding.GetInnerPadding(DiamondLetter);
+      Assert.Equal(11, result);
+    }
+
+    [Fact]
+    public void letter_F_returns_paddingLeft_0() {
+      var letter = 'F';
+      var diamondPadding = new DiamondLetter(letter);
+      int paddingLeft = diamondPadding.GetOuterPadding(DiamondLetter);
+      Assert.Equal(0, paddingLeft);
+    }
+
+    [Fact]
+    public void char_C_returns_innerPadding_5() {
+      var letter = 'C';
+      var middleDiamondPadding = new DiamondLetter(letter);
+      var result = middleDiamondPadding.GetInnerPadding(DiamondLetter);
+      Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void letter_C_for_diamond_F_returns_paddingLeft_3() {
+      var letter = 'C';
+      var diamondPadding = new DiamondLetter(letter);
+      int paddingLeft = diamondPadding.GetOuterPadding(DiamondLetter);
+      Assert.Equal(3, paddingLeft);
+    }
+
+    [Fact]
+    public void char_A_returns_innerPadding_0() {
+      var letter = 'A';
+      var middleDiamondPadding = new DiamondLetter(letter);
+      var result = middleDiamondPadding.GetInnerPadding(DiamondLetter);
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void letter_A_for_diamond_F_returns_paddingLeft_6() {
+      var letter = 'A';
+      var diamondPadding = new DiamondLetter(letter);
+      int paddingLeft = diamondPadding.GetOuterPadding(DiamondLetter);
+      Assert.Equal(6, paddingLeft);
+    }
+  }
+}
